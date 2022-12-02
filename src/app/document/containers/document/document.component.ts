18eb5989f1f3f7b1a95c2637860f45b2ab8ff61d
Tim Honermann <tim.honermann@peax.ch>
2022-12-02 14:17:26 +0100

    added menu

--- src/app/document/containers/document/document.component.ts
+++ src/app/document/containers/document/document.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
-  styleUrls: ['./document.component.scss']
+  styleUrls: ['./document.component.scss'],
 })
 export class DocumentComponent implements OnInit {
 
-  private init: DOMHighResTimeStamp = 0;
-
   fileUrl = '';
 
-  constructor(private readonly route: ActivatedRoute) { }
+  isLoading = true;
+
+  private init: DOMHighResTimeStamp = 0;
+
+  constructor(private readonly route: ActivatedRoute, private readonly cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.init = performance.now();
@@ -22,6 +24,8 @@ export class DocumentComponent implements OnInit {
 
   onLoaded(): void {
     console.log('loaded: ', performance.now() - this.init);
+    this.isLoading = false;
+    this.cdr.detectChanges();
   }
 
 }
--- src/app/document/document.module.ts
+++ src/app/document/document.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { DocumentComponent } from './containers/document/document.component';
 import { DocumentRoutingModule } from 'src/app/document/document-routing.module';
 import { PdfViewerComponent } from './components/pdf-viewer/pdf-viewer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 
@@ -13,7 +16,10 @@ import { PdfViewerComponent } from './components/pdf-viewer/pdf-viewer.component
   ],
   imports: [
     CommonModule,
-    DocumentRoutingModule
+    DocumentRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
   ]
 })
 export class DocumentModule { }
